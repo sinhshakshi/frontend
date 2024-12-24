9cdfd6ece30a3f4112b469dc93dbe59983f53c6c
sinhshakshi <sarkaritypingtest@gmail.com>
2024-12-24 19:22:39 +0530

    update

--- src/Typing/Result.js
+++ src/Typing/Result.js
@@ -197,7 +197,7 @@ const TypingPerformance = () => {
             <tbody>
              
                 <tr>
-                    <td>Total Typed Words</td>
+                    <td>Total Typed Keystrokes</td>
                     <td>{totaltyped}</td>
                 </tr>
                 <tr>
