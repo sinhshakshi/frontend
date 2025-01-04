5d63ee8f3ae39abfb24819c6e7caa6b9488d8e2d
sinhshakshi <sarkaritypingtest@gmail.com>
2025-01-04 13:22:22 +0530

    update canonical

--- src/Typinginfo/InfoPage.js
+++ src/Typinginfo/InfoPage.js
@@ -173,11 +173,7 @@ const InfoPage = () => {
            <meta property="og:image" content="https://testdesk.in/logo.png?v=1" />
         <meta property="og:url" content={`https://testdesk.in/course-page/${paramLink}`} />
         <meta name="twitter:image" content="https://testdesk.in/logo.png?v=1"  />
-
-        <link
-    rel="canonical"
-    href={`https://testdesk.in/course-page/${paramLink}`} // Replace with the actual URL
-  />
+        <link rel="canonical" href={`https://testdesk.in/course-page/${paramLink}`} />
 
       </Helmet>
     <div className="info-container">
